f6ba1bfd5d3032255f8c03fc06045fbe419088bf
arshil171 <ARSHILGONDALIYA5@GMAIL.COM>
2025-03-03 16:41:36 +0530

    javascript

---
+++ day3/index.js
@@ -0,0 +1,162 @@
+// datatype 
+
+// premmitive 
+
+// Number 
+// Boolean 
+// string 
+// BigInt 
+// undefined 
+// null 
+
+
+
+
+let number = 1000
+
+console.log(typeof number)
+
+
+let name = "Arshil"
+console.log (typeof name)
+
+// let name1 = true
+
+// console.log(typeof name)
+
+
+let num1 = true
+
+console.log(typeof num1)
+
+
+console.log(typeof undefined)
+
+let name3 = null
+console.log(typeof name3)
+
+// Note print all number 
+
+// let bigInt = 1233455453524352454634623153476547546874
+
+// console.log(bigInt)
+
+let bigInt = 1233455453524352454634623153476547546874n
+
+console.log(typeof bigInt)
+
+let bigInt1 = 1233455453524352454634623153476547546874n
+
+console.log(bigInt)
+
+
+// non-primitive
+
+// Array  [1,3,4,5,true,false,null,"arshil"]
+// Object
+
+let object = {
+    name6: "Arshil",
+    age: 22,
+    isStudent: true
+    // console.log(name6)
+}
+
+
+
+
+// function
+
+
+
+
+
+
+// Types of operator
+
+// arithmatic operator 
+
+// + (adding)
+// - (subtracting)
+// * (multiplying)
+// /  (divison)
+// % (Modulus)
+// ** (Exponent)
+// ++ {increment  (prifix)(postfix)}
+// -- {decrement  (prifix)(postfix)}
+
+
+let a= 10
+
+let b = 5
+
+console.log(a + b)
+
+console.log(a - b)
+
+console.log(a * b)
+
+console.log(a / b)
+
+console.log(a % b)
+
+console.log(a ** b)
+
+console.log(++a)
+
+console.log(--a)
+
+console.log(a--)
+
+console.log(a++)
+console.log(a)
+
+
+
+// Assignment operator
+
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+let add=0
+let x=5
+let y=10
+let sum 
+
+// add = add + 5
+add += 8
+
+
+console.log(add)
+
+console.log( y += x)
+
+console.log( y -= x)
+
+console.log(y *= x)
+
+console.log(y /= x)
+
+console.log(y %= x)
+
+// console.log(y ** x = sum)
+
+console.log(sum = x ** y)
+
+
+let a2 = 9
+
+let b2 = 4
+
+
+console.log(Math.round(a2/b2))
+console.log(a2/b2)
+
+console.log(Math.floor(a2/b2))
+
+console.log(Math.ceil(a2/b2))
